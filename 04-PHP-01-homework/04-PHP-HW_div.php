cd6d84a267371cdc3b0451bd15db462e5d00365d
mraiur <mraiur@gmail.com>
2015-08-05 12:07:00 +0300

    Homework 04 random color

--- 04-PHP-01-homework/04-PHP-HW_div.php
+++ 04-PHP-01-homework/04-PHP-HW_div.php
@@ -1,10 +1,7 @@
-
-<div style="background-color:pink" with=200; hight=200> </div> 
 <?php
-$q = rand();
 
-	echo $q;
-	echo "<br/>"
-	
-	
-	?> 
+$r = rand(0, 255);
+$g = rand(0, 255);
+$b = rand(0, 255);
+?>
+<div style="background-color: rgb(<?=$r?>, <?=$g?>, <?=$b?>); width: 200px; height: 200px; display:block;"> </div>
